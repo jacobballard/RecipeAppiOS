04c1f07877d4371c212fa12424fc48a4a3287587
William Ballard <jacob.ballard.1999@gmail.com>
2022-11-27 17:00:32 -0500

    Initial Commit

---
+++ Recipe/RecipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeApp.swift
+//  Recipe
+//
+//  Created by William Ballard on 11/27/22.
+//
+
+import SwiftUI
+
+@main
+struct RecipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
