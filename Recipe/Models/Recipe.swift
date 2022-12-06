407319c2a353e7cae0981c100e5da3fd02a1c99e
jacobballard <jacob.ballard.1999@gmail.com>
2022-12-06 16:50:37 -0500

    laptop

--- Recipe/Models/Recipe.swift
+++ Recipe/Models/Recipe.swift
@@ -6,6 +6,9 @@
 //
 
 import Foundation
+import FirebaseCore
+import FirebaseFirestore
+import FirebaseFirestoreCombineSwift
 
 final class Recipe : Codable, ObservableObject {
 
@@ -21,32 +24,35 @@ final class Recipe : Codable, ObservableObject {
 
     @Published var image : Data?
 
-
     enum CodingKeys: CodingKey {
+        
         case title
         case ingredients
         case instructions
         case tags
         case image
+        
     }
 
     required init(from decoder: Decoder) throws {
+        
         let container = try decoder.container(keyedBy: CodingKeys.self)
         title = try container.decode(String.self, forKey: .title)
-
-        //needs work
         ingredients = try container.decode([String].self, forKey: .ingredients)
         instructions = try container.decode([String].self, forKey: .instructions)
         tags = try container.decode([String].self, forKey: .tags)
         image = try container.decode(Data.self, forKey: .image)
+        
     }
     
     public func encode(to encoder: Encoder) throws {
+        
         var container = encoder.container(keyedBy: CodingKeys.self)
         try container.encode(title, forKey: .title)
         try container.encode(ingredients, forKey: .ingredients)
         try container.encode(instructions, forKey: .instructions)
         try container.encode(tags, forKey: .tags)
         try container.encode(image, forKey: .image)
+        
     }
 }
